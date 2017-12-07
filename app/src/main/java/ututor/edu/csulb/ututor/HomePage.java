b2f716239f2f900bc5dc212ad84a7af944ed8b1d
Aenah Ramones <aenah.ramones@gmail.com>
2017-12-07 13:33:08 -0800

    Initial commit

---
+++ app/src/main/java/ututor/edu/csulb/ututor/HomePage.java
@@ -0,0 +1,51 @@
+package ututor.edu.csulb.ututor;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class HomePage extends AppCompatActivity {
+
+    Calendar myCalendar;
+    EditText edittext;
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appointment_creation);
+
+        myCalendar = Calendar.getInstance();
+        edittext = (EditText) findViewById(R.id.Birthday);
+
+    }
+
+
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+}
