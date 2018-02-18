27bbd654e8e01278add717ecd46ae8ed664080a4
Nishant Saxena <25915502+scatterlava@users.noreply.github.com>
2018-02-17 17:47:07 -0800

    Made and Completed the Search Page with recycler view and card view with working search function.

---
+++ app/src/main/java/ututor/edu/csulb/ututor/RecyclerAdapter.java
@@ -0,0 +1,73 @@
+package ututor.edu.csulb.ututor;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private ArrayList<newItem> Data;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView itemImage;
+        public TextView itemTitle;
+        public TextView itemDetail;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemImage = (ImageView) itemView.findViewById(R.id.item_image);
+            itemTitle = (TextView) itemView.findViewById(R.id.item_title);
+            itemDetail = (TextView) itemView.findViewById(R.id.item_details);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Snackbar.make(v, "Click detected on item " + position,
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }
+            });
+        }
+    } //end of class ViewHolder
+
+    public RecyclerAdapter(ArrayList<newItem> DataList) {
+        Data = DataList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_layout,parent,false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int i) {
+        newItem currentItem = Data.get(i);
+        holder.itemImage.setImageResource(currentItem.getImage());
+        holder.itemTitle.setText(currentItem.getText1());
+        holder.itemDetail.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return Data.size();
+    }
+
+    public void filterList(ArrayList<newItem> filteredList)
+    {
+        Data = filteredList;
+        notifyDataSetChanged();
+    }
+}
