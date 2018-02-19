37d700f83e1310746c15eec95a6e78ca732eadab
titinguyen1106 <henrytran90@outlook.com>
2018-02-18 17:08:05 -0800

    Henry - finished the gprofile_bio.xml and profile_bio.xml

--- app/src/main/java/ututor/edu/csulb/ututor/gprofile_message.java
+++ app/src/main/java/ututor/edu/csulb/ututor/gprofile_message.java
@@ -1,8 +1,17 @@
 package ututor.edu.csulb.ututor;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Henry Tran on 2/12/2018.
  */
 
-public class gprofile_message {
+public class gprofile_message extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gprofile_message);
+    }
+
 }
