a7c1d89554314372c448e48b1aef5ec6df3bf6dc
Aenah Ramones <aenah.ramones@gmail.com>
2018-04-02 16:57:40 -0700

    Aenah - major change to appointments

--- app/src/main/java/ututor/edu/csulb/ututor/ScheduleAppointment.java
+++ app/src/main/java/ututor/edu/csulb/ututor/ScheduleAppointment.java
@@ -2,6 +2,7 @@ package ututor.edu.csulb.ututor;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
