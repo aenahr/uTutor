da840b9e697e59f047ab8481d4717aee15df8025
Lance McVicar <lancemcvicar@gmail.com>
2018-04-11 17:28:28 -0700

    Lance - Implmeneted Walk-In and a Skeleton Error Test Case for Search

--- app/src/main/java/ututor/edu/csulb/ututor/SearchList.java
+++ app/src/main/java/ututor/edu/csulb/ututor/SearchList.java
@@ -11,8 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class SearchList extends Fragment {
 
@@ -22,6 +27,7 @@ public class SearchList extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerAdapter adapter;
+    private  EditText userinput, searchuni;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -41,9 +47,12 @@ public class SearchList extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
-        EditText userinput = rootView.findViewById(R.id.userInputTutor);  //gets name from user
+        userinput = rootView.findViewById(R.id.userInputTutor);  //gets name from user
         //EditText userinputsubject = rootView.findViewById(R.id.searchsubject);
 
+        searchuni = rootView.findViewById(R.id.searchuniversity);
+
+       // searchuni.getText().toString();
         //gets subject from user
 
         userinput.addTextChangedListener(new TextWatcher() {  //does the stuff with the user input
@@ -60,32 +69,75 @@ public class SearchList extends Fragment {
             public void afterTextChanged(Editable editable) {
                 // Lance
                // this is where filtering happens
+
                 filter(editable.toString());
             }
         });
 
         return rootView;
     }
-
+    public TextView searchname;
     /** This method filters entries */
     private void filter(String text) {
-        if(text != null && !text.isEmpty()) {
-            ArrayList<NewItem> filteredList = new ArrayList<>();
 
-            String[] name = text.split(" ");
+        JSONObject response = null;
+        try { //Will internally test for Current Email and Password Matches, Returns Success if successful
+            String[] name =  userinput.getText().toString().split(" ");
             String firstname = name[0];
             String lastname = name[1];
+            response = new ServerRequester().execute("search.php", "whatever",
+                    "firstName", firstname.toLowerCase(),
+                    "lastName", "eh",
+                    "subject", "eh",
+                    "university",  "eh",
+                    "rating",  "eh").get();
+            if (response == null) {//Something went horribly , JSON failed to be formed meaning something happened in the server requester
+            } else if (!response.isNull("error")) {//Some incorrect information was sent, but the server and requester still processed it
+                //TODO Handle Server Errors
+                switch(response.get("error").toString()) {
+                    case "-1": //Email Password Combo not in the Database
+                        break;
+                    case "-2":  //Select Query failed due to something dumb
+                        // Print out response.get("errormessage"), it'll have the mysql error with it
 
-            for (NewItem item : DataList) {
-                if (item.getfirstname().toLowerCase().contains(firstname.toLowerCase())) {
-                    filteredList.add(item);
+                        break;
+                    case "-3": //Update Query Failed Due to New Email is already associated with another account
+                        break;
+                    case "-4":  //Update Query Failed Due to Something Else Dumb that I haven't handled yet,
+                        // Print out response.get("errormessage"), it'll have the mysql error with it
+                        break;
+                    default:    //Some Error Code was printed from the server that isn't handled above
+
+                        break;
                 }
-            }
-            adapter.filterList(filteredList);
-        } else {
-            ArrayList<NewItem> filteredList = new ArrayList<>();
-            adapter.filterList(filteredList);
+            }}
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch(JSONException e) {
         }
+
+
+
+
+//        if(text != null && !text.isEmpty()) {
+//            ArrayList<NewItem> filteredList = new ArrayList<>();
+//
+//            String[] name = text.split(" ");
+//            String firstname = name[0];
+//            String lastname = name[1];
+//
+//            for (NewItem item : DataList) {
+//                if (item.getfirstname().toLowerCase().contains(firstname.toLowerCase())) {
+//                    filteredList.add(item);
+//                }
+//            }
+//            adapter.filterList(filteredList);
+//        } else {
+//            ArrayList<NewItem> filteredList = new ArrayList<>();
+          //  adapter.filterList(filteredList);
+        //}
     }
 
     private void CreateDataList() {
