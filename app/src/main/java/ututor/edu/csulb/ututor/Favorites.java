ef58e99568365d99b26bc07f0a211011adce6e91
Lance McVicar <lancemcvicar@gmail.com>
2018-05-12 15:30:24 -0700

    Lance - Unfavorite and fetchUser request added

--- app/src/main/java/ututor/edu/csulb/ututor/Favorites.java
+++ app/src/main/java/ututor/edu/csulb/ututor/Favorites.java
@@ -106,27 +106,88 @@ public class Favorites extends Fragment implements AdapterView.OnItemSelectedLis
                         public void onClick(DialogInterface dialog, int which) {
 
                             // TODO: database remove favorite relationship by email
-                            //currentUser.getEmail(); // favoritor
-                            //emails.get(position); // favoritee
-                            names.remove(position);
-                            emails.remove(position);
-                            adapter.notifyDataSetChanged();
+                            JSONObject response = null;
+                            try {
+                                response = new ServerRequester().execute("unFavorite.php", "whatever"
+                                        ,"favoritorEmail", currentUser.getEmail()
+                                        ,"favoriteeEmail", emails.get(position)
+                                ).get();
+                                if (response == null) {//Something went horribly wrong, JSON failed to be formed meaning something happened in the server requester
+
+                                } else if (!response.isNull("error")) {//Some incorrect information was sent, but the server and requester still processed it
+                                    switch (response.get("error").toString()) {
+                                        default:    //Some Error Code was printed from the server that isn't handled above
+
+                                            break;
+                                    }
+                                } else { //Everything Went Well
+                                    names.remove(position);
+                                    emails.remove(position);
+                                    adapter.notifyDataSetChanged();
+                                }
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            } catch (ExecutionException e) {
+                                e.printStackTrace();
+                            } catch (JSONException e){
+                                e.printStackTrace();
+                            }
                         }});
                     adb.show();
 
                 }
                 else{
                     // TODO: database fetch user by email
-                    // look at the profile
-                    Toast.makeText(getActivity(), names.get(position), Toast.LENGTH_SHORT).show();
+                    JSONObject response = null;
+                    try {
+                        response = new ServerRequester().execute("fetchUser.php", "whatever",
+                                "email", emails.get(position)
+                        ).get();
+                        if (response == null) {//Something went horribly wrong, JSON failed to be formed meaning something happened in the server requester
+                        } else if (!response.isNull("error")) {//Some incorrect information was sent, but the server and requester still processed it
+                            //TODO Handle Server Errors
+                            switch (response.get("error").toString()) {
+                                case "-1": //Email Password Combo not in the Database
+                                    break;
+                                case "-2":  //Select Query failed due to something dumb
+                                    // Print out response.get("errormessage"), it'll have the mysql error with it
 
-//            Intent i = new Intent(getActivity(), GenericProfile.class);
-//            i.putExtra("email", );
-//            startActivity(i);
+                                    break;
+                                case "-3": //Update Query Failed Due to New Email is already associated with another account
+                                    break;
+                                case "-4":  //Update Query Failed Due to Something Else Dumb that I haven't handled yet,
+                                    // Print out response.get("errormessage"), it'll have the mysql error with it
+                                    break;
+                                default:    //Some Error Code was printed from the server that isn't handled above
 
+                                    break;
+                            }
+                        } else { //Everything Went Well
+                            User otherUser = new User();
+                            otherUser.setFirstName(response.get("firstName").toString());
+                            otherUser.setLastName(response.get("lastName").toString());
+                            otherUser.setEmail(response.get("email").toString());
+                            otherUser.setUniversity(response.get("university").toString());
+                            otherUser.setRating(Float.parseFloat(response.get("averageRating").toString()));
+                            otherUser.setDescription(response.get("userDescription").toString());
+                            otherUser.setNumProfilePic(Integer.parseInt(response.get("profilePic").toString()));
+                            otherUser.setPhoneNumber(response.get("phoneNumber").toString());
+                            Intent i = new Intent(getActivity(), GenericProfile.class);
+                            i.putExtra("currentUser", currentUser);
+                            i.putExtra("otherUser", otherUser);
+                            getActivity().startActivity(i);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (JSONException e){
+                        e.printStackTrace();
+                    }
                 }
-            }
-        });
+//                    Toast.makeText(getActivity(), names.get(position), Toast.LENGTH_SHORT).show();
+                }
+            });
 
         edit_fav = (Button) rootView.findViewById(R.id.editFav);
         edit_fav.setOnClickListener(new View.OnClickListener() {
